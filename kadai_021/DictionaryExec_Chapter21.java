b809f836d292571babf7190c67dfdf7eea748af2
Kimihikodesu <kimihiko20040115@gmail.com>
2025-05-17 21:09:42 +0900

    Add files via upload

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,19 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+		 Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
+
+	       
+	        String[] wordsToSearch = { "apple", "banana", "grape", "orange" };
+
+	        
+	        for (String word : wordsToSearch) {
+	            String result = dictionary.search(word);
+	            System.out.println(result);
+	        }
+
+	}
+
+}
